7ae50cf1da0842c7a3d2a69c03aba983079c54c0
Slavus54 <x.miros@yandex.ru>
2023-12-31 11:32:40 +0700

    Second deployment

--- src/components/hoc/Welcome.tsx
+++ src/components/hoc/Welcome.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import cards from '../../env/cards.json'
 
 const Welcome: React.FC = () => {
     return (
         <>          
             <h1 className='welcome__headline'>Modus Vivendi</h1>
             <h3 className='lazy-text'>Платформа для свободных людей</h3>
+            <div className='items'>
+                {cards.map(el =>
+                    <div className='item panel'>
+                        <h2>{el.title}</h2>
+                        <p className='pale'>{el.category}</p>
+                        <p>{el.text}</p>
+                    </div>    
+                )}
+            </div>
         </>
     )
 }
